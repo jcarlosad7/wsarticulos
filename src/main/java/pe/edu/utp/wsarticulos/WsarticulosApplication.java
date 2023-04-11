d56a6c38f971212525ebadf33ec723d42aa1b25c
Juan Carlos Arcila Diaz <incanatoapps@gmail.com>
2023-04-10 20:29:24 -0500

    Commit initial

---
+++ src/main/java/pe/edu/utp/wsarticulos/WsarticulosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.wsarticulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsarticulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsarticulosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/wsarticulos/WsarticulosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.wsarticulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsarticulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
